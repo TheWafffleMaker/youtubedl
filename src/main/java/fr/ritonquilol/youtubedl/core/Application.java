bc42962848d1950a96d3886df11a5ca6132b90b2
TheWafffleMaker <bouvet.henri@gmail.com>
2018-09-24 20:04:16 +0200

    Dynamic progress

--- src/main/java/fr/ritonquilol/youtubedl/core/Application.java
+++ src/main/java/fr/ritonquilol/youtubedl/core/Application.java
@@ -1,6 +1,9 @@
 package fr.ritonquilol.youtubedl.core;
 
 import javax.swing.*;
+import javax.swing.text.DefaultCaret;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * youtube-dl.exe simplifier
@@ -18,8 +21,9 @@ public class Application {
         JTextField pathArea = WindowFactory.createTextField(300, 20, window.getX()+75, window.getY()+80, System.getProperty("user.home") + "\\Downloads\\");
         JCheckBox audioOnly = WindowFactory.createCheckBox(50, 115, "Audio only");
         JCheckBox playlist = WindowFactory.createCheckBox(150, 115, "Playlist");
-        JScrollPane progress = WindowFactory.createTextAreaWithScroll(500, 150, 40, 150, "");
-
+        JScrollPane progressScroll = WindowFactory.createTextAreaWithScroll(500, 150, 40, 150, "");
+        JTextArea progress = (JTextArea) progressScroll.getViewport().getComponent(0);
+        ((DefaultCaret)progress.getCaret()).setUpdatePolicy(DefaultCaret.OUT_BOTTOM);
 
         window.add(WindowFactory.createLabel(window.getX()+30, window.getY()+57, "URL  :"));
         window.add(urlArea); // URL
@@ -27,9 +31,9 @@ public class Application {
         window.add(pathArea); // PATH
         window.add(audioOnly);
         window.add(playlist);
-        window.add(progress);
-        window.add(WindowFactory.createButton(120, 30, window.getX()+400, window.getY()+55, "Download", new Downloader.DownloadListener((JTextArea) progress.getViewport().getComponent(0))));
-        window.add(WindowFactory.createButton(120, 30, window.getX()+40, window.getY()+315, "Update", new Updater.UpdateListener((JTextArea) progress.getViewport().getComponent(0))));
+        window.add(progressScroll);
+        window.add(WindowFactory.createButton(120, 30, window.getX()+400, window.getY()+55, "Download", new Downloader.DownloadListener((JTextArea) progress)));
+        window.add(WindowFactory.createButton(120, 30, window.getX()+40, window.getY()+315, "Update", new Updater.UpdateListener(progress)));
         window.setVisible(true);
 
     }
